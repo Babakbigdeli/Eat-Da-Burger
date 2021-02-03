f61bf1ade60fddeab5bec6a333c74dc908047a98
Babakbigdeli <babakbigdeli@gmail.com>
2021-02-03 12:24:05 +0000

    connection defined

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// Setting up MySQL connection.
+const mysql = require("mysql");
+
+//Establishing connection (Heroku and Local)
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  })
+}
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+// Exporting connection
+module.exports = connection;
