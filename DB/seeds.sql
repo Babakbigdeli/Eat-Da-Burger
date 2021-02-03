63f3d10d12e74c0acde80dce020dc7224192b32f
Babakbigdeli <babakbigdeli@gmail.com>
2021-02-03 11:59:49 +0000

    Database Created

---
+++ DB/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Zinger Chickenburger');
+INSERT INTO burgers (burger_name) VALUES ('Beef Burger');
