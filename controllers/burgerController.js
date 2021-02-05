34ac1593ca08a717915c6a0328e301186a5a578c
Babakbigdeli <babakbigdeli@gmail.com>
2021-02-05 16:28:16 +0000

    home page route

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,3 +3,15 @@ const express = require('express')
 const router = express.Router();
 const burger = require('../models/burger.js');
 
+//Creating Routes starting with home page
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var burgerData = {
+        burgers: data
+      };
+      res.render("index", burgerData);
+    });
+});
+
+
+
